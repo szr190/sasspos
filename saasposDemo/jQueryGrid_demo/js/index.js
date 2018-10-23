f286faba11a5054a02d39067d83ba18149a9fca6
szr190 <szr190@163.com>
2018-10-23 16:20:58 +0800

    update

--- saasposDemo/jQueryGrid_demo/js/index.js
+++ saasposDemo/jQueryGrid_demo/js/index.js
@@ -10,12 +10,12 @@ function pageInit(){
 				datatype : "json",//请求数据返回的类型。可选json,xml,txt
 				colNames : [ 'Inv No', 'Date', 'Client', 'Amount', 'Tax','Total', 'Notes' ],//jqGrid的列显示名字
 				colModel : [ //jqGrid每一列的配置信息。包括名字，索引，宽度,对齐方式.....
-				             {name : 'id',index : 'id',width : 55}, 
-				             {name : 'invdate',index : 'invdate',width : 90}, 
+				             {name : 'id',index : 'id',width : 100,hidden:true}, 
+				             {name : 'invdate',index : 'invdate',width : 100}, 
 				             {name : 'name',index : 'name asc, invdate',width : 100}, 
-				             {name : 'amount',index : 'amount',width : 80,align : "right"}, 
-				             {name : 'tax',index : 'tax',width : 80,align : "right"}, 
-				             {name : 'total',index : 'total',width : 80,align : "right"}, 
+				             {name : 'amount',index : 'amount',width : 100,align : "right"}, 
+				             {name : 'tax',index : 'tax',width : 100,align : "right"}, 
+				             {name : 'total',index : 'total',width : 100,align : "right"}, 
 				             {name : 'note',index : 'note',width : 150,sortable : false} 
 				           ],
 				rowNum : 10,//一页显示多少条
